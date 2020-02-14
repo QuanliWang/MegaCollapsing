34f23b0c0ce50ee1e4eb75976e98e0b5d1c8cc96
Quanli Wang <quanliwang20@hotmail.com>
2020-02-13 20:54:31 -0500

    reimplement burden.test to enable parallel foreach

--- R/megamatrix.R
+++ R/megamatrix.R
@@ -1,4 +1,4 @@
-exclude.genes <- function(input.data, exclude.file) {
+exclude.samples.by.genes <- function(input.data, exclude.file) {
   if (!file.exists(exclude.file)) {
     stop("exlude gene file not found")
   }
@@ -19,53 +19,45 @@ exclude.genes <- function(input.data, exclude.file) {
 }
 
 as.mega.matrix<- function(gene.sets, input.data) {
-  mega.gene.names<- sapply(gene.sets, function(x) x[1])
-  common_sample_list <- rownames(input.data$sample.list)
+  sample.list <- colnames(input.data$non.syn)
+  mega.gene.names <- rownames(gene.sets$mat)
+
+  gene.names.from.gene.sets <- colnames(gene.sets$mat)
+  gene.names.from.collapsing.matrix <- rownames(input.data$non.syn)
+
   has.syn <- "syn" %in% names(input.data)
-  if (has.syn) {
-    mega.syn <- matrix(NA,nrow = length(gene.sets), ncol = length(common_sample_list))
-  }
-  mega.non.syn <- matrix(NA,nrow = length(gene.sets), ncol = length(common_sample_list))
-  missing_genes = list()
-  for (i in 1:length(gene.sets)) {
-    if (i %% 10 == 0) {
-      print(paste("Processing ", i, " out of ", length(gene.sets), " megagenes..."))
-    }
-    genes <- gene.sets[[i]][3:length(gene.sets[[i]])]
-    common_genes <- intersect(genes,rownames(input.data$non.syn))
-    missing_genes[[mega.gene.names[i]]] <- setdiff(genes,rownames(input.data$non.syn))
-    if (length(common_genes)>0) {
-      if (has.syn) {
-        temp <- input.data$syn[common_genes,]
-        temp[temp > 0] <- 1
-        mega.syn[i,] <- colSums(temp)
-      }
-      temp <- input.data$non.syn[common_genes,]
-      temp[temp > 0] <- 1
-      mega.non.syn[i,] <- colSums(temp)
-    }
-  }
-  if (has.syn) {
-    mega.syn <- as.data.frame(mega.syn)
-    colnames(mega.syn) <- common_sample_list
-    rownames(mega.syn) <- mega.gene.names
-  }
-  mega.non.syn <- as.data.frame(mega.non.syn)
-  colnames(mega.non.syn) <- common_sample_list
-  rownames(mega.non.syn) <- mega.gene.names
 
-  adjusted.gene.set.size <- unlist(lapply(gene.sets,length))  - 2 - unlist(lapply(missing_genes,length))
+  common.genes <- intersect(gene.names.from.gene.sets, gene.names.from.collapsing.matrix)
+  if (length(common.genes) == 0) {
+      stop("no common genes were found among collapsing matrix and gene sets.")
+  }
+
+  gene.index <- match(common.genes, gene.names.from.collapsing.matrix)
+  if (has.syn) {
+      input.data$syn <- input.data$syn[gene.index,]
+      input.data$syn[input.data$syn > 0] <- 1
+  }
+  input.data$non.syn <- input.data$non.syn[gene.index,]
+  input.data$non.syn[input.data$non.syn > 0] <- 1
+
+  gene.index <- match(common.genes, gene.names.from.gene.sets)
+  gene.sets$mat <- gene.sets$mat[,gene.index]
+  gene.sets$mat[gene.sets$mat] <-1
+  gene.sets$mat[!gene.sets$mat] <-0
+  if (has.syn) {
+      mega.syn <- gene.sets$mat %*% input.data$syn
+  }
+  mega.non.syn <- gene.sets$mat %*% input.data$non.syn
 
   if (has.syn) {
-    return(list(mega.syn = mega.syn, mega.non.syn = mega.non.syn, missing.genes = missing_genes,
-                adjusted.gene.set.size = adjusted.gene.set.size))
+    return(list(mega.syn = mega.syn, mega.non.syn = mega.non.syn, sample.list = sample.list))
   } else {
-    return(list(mega.non.syn = mega.non.syn, missing.genes = missing_genes,
-                adjusted.gene.set.size = adjusted.gene.set.size))
+    return(list(mega.non.syn = mega.non.syn, sample.list = sample.list))
   }
 }
 
-burden.test <- function(sample.list, gene.sets, syn.mat, non.syn.mat, X = NULL, Y = NULL) {
+burden.test <- function(sample.list, gene.sets, syn.mat, non.syn.mat, X = NULL, Y = NULL, ncores = 1) {
+
   if (is.null(Y)) {
     if (is.element("Status",names(sample.list))) {
       Y <- sample.list[,'Status'] - 1
@@ -77,7 +69,6 @@ burden.test <- function(sample.list, gene.sets, syn.mat, non.syn.mat, X = NULL, 
   median.syn.control <- median(rowSums(syn.mat[,Y == 0]))
   median.non.syn.case <- median(rowSums(non.syn.mat[,Y == 1]))
   median.non.syn.control <- median(rowSums(non.syn.mat[,Y == 0]))
-
 
   X0 <- NULL
   if (!is.null(X)) {
@@ -93,30 +84,71 @@ burden.test <- function(sample.list, gene.sets, syn.mat, non.syn.mat, X = NULL, 
   }
 
   p <- dim(data.base)[2] + 2
-  mega.gene.names <- lapply(gene.sets, function (x) x[[1]])
+  mega.gene.names <- rownames(gene.sets$mat)
 
-  p_values <- matrix(NA,length(gene.sets),p)
+  p_values <- matrix(NA,length(mega.gene.names),p)
   rownames(p_values) <- mega.gene.names
   colnames(p_values) <- c('(Intercept)', 'non.syn', 'syn', names(data.base)[-1])
 
-  betas <- matrix(NA,length(gene.sets),p)
+  betas <- matrix(NA,length(mega.gene.names),p)
   rownames(betas) <- mega.gene.names
   colnames(betas) <- c('(Intercept)', 'non.syn', 'syn', names(data.base)[-1])
-
   fm <- as.formula(paste("Y~", paste(colnames(p_values)[-1], collapse="+")))
-  for (i in 1:length(gene.sets)) {
-    if (i %% 10 == 0) {
-      print(paste("Processing ", i, " out of ", length(gene.sets), " megagenes..."))
-    }
-    non.syn <- t(non.syn.mat[i,])
-    syn <- t(syn.mat[i,])
-    data.i <- cbind(data.base, non.syn , syn)
-    names(data.i) <- c(names(data.base), "non.syn", "syn")
-    try({
-      coef <- summary(glm( fm, data = data.i, family = binomial(link = logit)))$coef
-      p_values[i,rownames(coef)] <- coef[,4]
-      betas[i,rownames(coef)] <- coef[,1]
-    }, silent=TRUE)
+
+  if (ncores >1) {
+      # Set up parallel backend
+      cl.create <- FALSE
+      if (foreach::getDoParWorkers() > 1) {
+        if (foreach::getDoParWorkers() != ncores) {
+          message(paste("Parallel backend already registered and is inconsistent with ncores."))
+        }
+        ncores <- getDoParWorkers()
+      } else {
+          cl.create <- TRUE
+          cl <- parallel::makeCluster(ncores, outfile = "")
+          doParallel::registerDoParallel(cl)
+          on.exit({
+            parallel::stopCluster(cl)
+            foreach::registerDoSEQ()
+          })
+      }
+
+      itercount <- iterators::icount(length(mega.gene.names))
+      out <- suppressWarnings(foreach::foreach(i = itercount, .packages = "MegaCollapsing", .errorhandling="pass") %dopar% {
+                if (i %% 10 == 0) {
+                      print(paste("Processing ", i, " out of ", length(mega.gene.names), " megagenes..."))
+                }
+                non.syn <- non.syn.mat[i,]
+                syn <- syn.mat[i,]
+                data.i <- cbind(data.base, non.syn , syn)
+                names(data.i) <- c(names(data.base), "non.syn", "syn")
+                try({
+                    coef <- summary(glm( fm, data = data.i, family = binomial(link = logit)))$coef
+                    p_value <- coef[,4]
+                    beta <- coef[,1]
+                   list(p_value, beta, i)
+                }, silent=TRUE)
+              })
+      indexes <- sapply(out,function(x) x[[3]])
+      for (i in 1:length(indexes)) {
+        p_values[indexes[i],names(out[[i]][[2]])] <- out[[i]][[1]]
+        betas[indexes[i],names(out[[i]][[2]])] <- out[[i]][[2]]
+      }
+  } else {
+      for (i in 1:length(mega.gene.names)) {
+        if (i %% 10 == 0) {
+          print(paste("Processing ", i, " out of ", length(mega.gene.names), " megagenes..."))
+        }
+        non.syn <- non.syn.mat[i,]
+        syn <- syn.mat[i,]
+        data.i <- cbind(data.base, non.syn , syn)
+        names(data.i) <- c(names(data.base), "non.syn", "syn")
+        try({
+          coef <- summary(glm( fm, data = data.i, family = binomial(link = logit)))$coef
+          p_values[i,rownames(coef)] <- coef[,4]
+          betas[i,rownames(coef)] <- coef[,1]
+        }, silent=TRUE)
+      }
   }
   return(list(p_values = p_values, betas = betas, median.syn.case = median.syn.case,
               median.syn.control = median.syn.control, median.non.syn.case = median.non.syn.case,
